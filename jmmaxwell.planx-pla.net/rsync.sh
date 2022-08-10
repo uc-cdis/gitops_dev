ae16ef1c5930ace938a0dabfae49725636130ad3
J Montgomery Maxwell <jmontgomerymaxwell@Js-MacBook-Pro.local>
2022-08-10 13:34:48 -0500

    update dev env

--- jmmaxwell.planx-pla.net/rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-folder="$(dirname "$(realpath "$0")")"
-command=(rsync -avz --exclude '*/data-volume/*' "${folder}/" devplanetv1@cdistest.csoc:cdis-manifest/dev.planx-pla.net/)
-echo "${command[@]}"
-${command[@]}
